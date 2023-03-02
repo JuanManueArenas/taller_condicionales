072b6f06cff5bea514d6c4413b120e9298ec9fc5
JuanManueArenas <juanarenas@colegioguanenta.edu.co>
2023-03-02 16:32:41 -0500

    se agrego el programa y se actualizo el diagrama

--- ejercicio_1/ejercicio_1.py
+++ ejercicio_1/ejercicio_1.py
@@ -1,23 +1,37 @@
-# programa que lea las coordenadas cartesianas
-print("------------------------------------------")
-print("---NUMERO PARA INVERTIR:---")
-print("------------------------------------------")
-## input 
-x = float(input("Ingrese la coordenada x: "))
-y = float(input("Ingrese la coordenada y: "))
+# Programa para leer cordenadas de un plano cartesiano e indicar el cuadrante
 
-#processing
-if x == 0 and y == 0:
-    print("El punto se encuentra en el origen.")
-elif x == 0:
-    print("El punto se encuentra sobre el eje y.")
-elif y == 0:
-    print("El punto se encuentra sobre el eje x.")
-elif x > 0 and y > 0:
-    print("El punto se encuentra en el primer cuadrante.")
-elif x < 0 and y > 0:
-    print("El punto se encuentra en el segundo cuadrante.")
-elif x < 0 and y < 0:
-    print("El punto se encuentra en el tercer cuadrante.")
+
+print("-------------------------------------------------------")
+print("Programa para indicar el cuadrante de  unas coordenadas")
+print("-------------------------------------------------------")
+
+#input
+
+x = int(input("Digite la coordenada x: "))
+y = int(input("Digite la coordenada y: "))
+
+# processing
+
+if x == 0:
+    if y == 0:
+        msj = ("Las coordenadas corresponden al origen")
+    else:
+        msj = ("Las coordenadas corresponden al eje y")
 else:
-    print("El punto se encuentra en el cuarto cuadrante.")
+    if y == 0:
+        msj = ("Las coordenadas corresponden al eje x")
+    else:
+        if x > 0:
+            if y > 0:
+                msj = ("Las coordenadas corresponden al cuadrante 1")
+            else:
+                msj = ("Las coordenadas corresponden al cuadrante 4")
+        else:
+            if y < 0:
+                msj = ("Las coordenadas corresponden al cuadrante 3")
+            else:
+                msj = ("Las coordenadas corresponden al cuadrante 2")
+# output
+
+print (msj)
+
