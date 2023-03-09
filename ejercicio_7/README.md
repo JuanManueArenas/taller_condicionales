1a25222630d610b06f96fdf10a65c613a04d43cd
JuanManueArenas <juanarenas@colegioguanenta.edu.co>
2023-03-09 16:25:49 -0500

    se agrego el diagrama y el programa y se agrego el README

---
+++ ejercicio_7/README.md
@@ -0,0 +1,9 @@
+# determinar si dados tres números enteros
+Programa para determinar si dados tres números enteros, la suma de los dos
+primeros es igual al tercero.
+
+# Diseño 
+
+## Diagrama de flujo 
+
+![Diagrama de flujo](diagrama.png "Diagrama de flujo")
---
+++ ejercicio_7/ejercicio_7.py
@@ -0,0 +1,17 @@
+
+print("------------------------------------------")
+print("-------------El resultado es:-------------")
+print("------------------------------------------")
+
+
+#input
+num1 = int(input("Ingrese el primer número: "))
+num2 = int(input("Ingrese el segundo número: "))
+num3 = int(input("Ingrese el tercer número: "))
+#processing
+if num1 + num2 == num3:
+    print("La suma de los dos primeros números es igual al tercer número")
+else:
+    print("La suma de los dos primeros números no es igual al tercer número")
+
+
